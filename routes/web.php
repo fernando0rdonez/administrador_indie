b35407784d2adecc1cd040d5d9d6986981fd9e15
Fernando Ordoñez <ferjordo2011@gmail.com>
2021-01-05 13:37:40 -0500

    Cambios web

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [App\Http\Controllers\PedidoController::class, 'index'])->name('index');
+
 
 Auth::routes();
 
